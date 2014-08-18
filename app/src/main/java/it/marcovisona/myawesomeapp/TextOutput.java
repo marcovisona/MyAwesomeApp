748e75708f3f86291a17ceca900c60c1de0bcb40
Marco Visonà <marco@interno27.it>
2014-08-18 21:02:59 +0200

    initial commit

---
+++ app/src/main/java/it/marcovisona/myawesomeapp/TextOutput.java
@@ -0,0 +1,10 @@
+package it.marcovisona.myawesomeapp;
+
+/**
+ * Created by giorgia on 18/08/14.
+ */
+public class TextOutput {
+    public static String printText(){
+        return "Hello, text!";
+    }
+}
